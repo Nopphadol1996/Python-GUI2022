e83adfdea0b1baa5eab7aa805d34d00e2d18c7eb
Nopphadol1996 <89741372+Nopphadol1996@users.noreply.github.com>
2023-02-19 09:11:57 +0700

    EP5
    Python GUI 2022  - EP.5
    
    EP นี้ลุงจะแนะนำเกี่ยวกับ การใช้งานตารางในการ GUI เพื่อดึงข้อมูลจากไฟล์ csv นำมาโชว์ในตาราง รวมถึงการทำความเข้าใจเรื่องการบันทึกเลข reference เพื่ออ้างอิงรายการที่ถูกบันทึกไว้
    
    - Python UUID
    - Treeview ตารางใน GUI
    - ดึงข้อมูลจาก csv นำมาโชว์ในตาราง
    - เพิ่ม ลบ แก้ไขข้อมูลใน csv

--- Python GUI2022/GUI-Wikipedia.py
+++ Python GUI2022/GUI-Wikipedia.py
@@ -3,6 +3,7 @@ from tkinter import ttk,messagebox
 import csv
 import wikipedia
 from datetime import datetime
+import webbrowser
 
 def writetocsv(data):
     with open('data.csv','a',newline='',encoding='utf-8') as file:
@@ -11,7 +12,7 @@ def writetocsv(data):
 
 GUI = Tk()
 GUI.title('โปรแกรมจัดการ layout')
-GUI.geometry('800x600')
+GUI.geometry('1200x600')
 
 ####### TAB Setting 
 Tab = ttk.Notebook(GUI)
@@ -19,17 +20,23 @@ Tab.pack(fill=BOTH,expand=1)
 
 T1 = Frame(Tab)
 T2 = Frame(Tab)
+T3 = Frame(Tab)
 
 ############ TAB 1 กุ้ง ###############
 
 FONT1 = ('Angsana New',25)
 FONT2 = ('impact',25)
 
+# set Photo
 icon_tab1 = PhotoImage(file='tab1.png')
 icon_tab2 = PhotoImage(file='tab2.png')
+icon_tab3 = PhotoImage(file='tab3.png')
+
+latte = PhotoImage(file='latte.png')
 
 Tab.add(T1,text='กุ้ง',image=icon_tab1,compound='left')
 Tab.add(T2,text='Wiki',image=icon_tab2,compound='left')
+Tab.add(T3,text='CAFE',image=icon_tab3,compound='left')
 
 L1 = Label(T1,text='กรอกจำนวนกุ้ง (กิโลกรัม)',font=FONT1).pack()
 
@@ -63,15 +70,19 @@ v_search = StringVar()
 E2 = ttk.Entry(T2,textvariable=v_search,font=FONT1).pack(pady=10)
 
 wikipedia.set_lang("th") # ตั้งเป็นภาษาไทย
+v_link = StringVar() # สร้างตัวแปรเพื่อเอาไปใช้งานในฟังก์ชั่นอื่นต่อ
+
 def Search():
 
     try:
-
         search = v_search.get() # ดึงข้อความจากช่องกรอก 
-        text = wikipedia.summary(search)
+        # text = wikipedia.summary(search)
+        text = wikipedia.page(search)
         # print(text)
-        # v_result.set(text[:500])
-        v_result.set(text[:1500]) # กำหนดข้อความให้น้อยกว่า 1500 ตัวอักษร
+        v_result.set(text.content[:800]) # กำหนดข้อความให้น้อยกว่า 1500 ตัวอักษร
+        print('Link',text.url)
+        v_link.set(text.url)
+        B3 = ttk.Button(T2,text='อ่านต่อ',command=readmore).pack()
     except:
         v_result.set('ไม่มีข้อมูล กรุณาค้นหาใหม่')
 
@@ -80,8 +91,89 @@ def Search():
 
 B2 = ttk.Button(T2,text='search',image=icon_tab2,compound='left',command=Search).pack(ipadx=40)
 
+def readmore():
+    webbrowser.open(v_link.get())
+
 v_result = StringVar()
 v_result.set('-------Result--------')
 result = Label(T2,textvariable=v_result,wraplength=550).pack() # wraplength=550 ตัดคำขึ้นบรรทัดใหม่
+
+############ TAB 3 Ca'fe ###############
+
+Bfont = ttk.Style()
+Bfont.configure('TButton',font=('Angsna New',11))
+
+CF1 = Frame(T3)
+CF1.place(x=50,y=100)
+
+allmenu = {}
+
+product = {'latte':{'name':'ลาเต้','price':30},
+            'cappuccino':{'name':'คาปูชิโน','price':35},
+            'espresso':{'name':'เอสเปรสโซ','price':40}}
+
+def UpdateTable():
+    table.delete(*table.get_children())
+    for i ,m in enumerate(allmenu.values(),start=1):
+        table.insert('','end',value=[i,m[0],m[1],m[2],m[3]])
+
+
+def AddMenu(name = 'latte'):
+    #name = 'latte'
+    if name not in allmenu:
+
+        allmenu[name] = [product[name]['name'],product[name]['price'],1,product[name]['price']]
+        print(allmenu)
+    else:
+        qaun = allmenu[name][2] + 1
+        total = qaun * product[name]['price']
+        allmenu[name] = [product[name]['name'],product[name]['price'],qaun,total]
+        print(allmenu)
+
+def Menu1():
+    AddMenu('latte')
+    UpdateTable()
+
+def Menu2():
+    AddMenu('cappuccino')
+    UpdateTable()
+
+def Menu3():
+    AddMenu('espresso')
+    UpdateTable()
+
+
+
+
+
+# ROW0
+B = ttk.Button(CF1,text='ลาเต้',image=latte,compound='top',command=Menu1).grid(row=0,column=0,ipadx=20,ipady=10)
+B = ttk.Button(CF1,text='คาปูชิโน',image=icon_tab3,compound='top',command=Menu2).grid(row=0,column=1,ipadx=20,ipady=10)
+B = ttk.Button(CF1,text='เอสเปรสโซ่่',image=icon_tab3,compound='top',command=Menu3).grid(row=0,column=2,ipadx=20,ipady=10)
+
+# ROW1
+
+B = ttk.Button(CF1,text='ชาเขียว',image=icon_tab3,compound='top').grid(row=1,column=0,ipadx=20,ipady=10)
+B = ttk.Button(CF1,text='ชาเย็น',image=icon_tab3,compound='top').grid(row=1,column=1,ipadx=20,ipady=10)
+B = ttk.Button(CF1,text='ชาร้อน',image=icon_tab3,compound='top').grid(row=1,column=2,ipadx=20,ipady=10)
+
+CF2 = Frame(T3)
+CF2.place(x=500,y=100)
+
+header = ['No.','title','price','quantity','total']
+hwidth = [50,200,100,100,100]
+
+
+table = ttk.Treeview(CF2,columns=header, show='headings',height=15)
+table.pack()
+
+for hd,hw in zip(header,hwidth):
+
+
+    table.column(hd,width=hw)
+    table.heading(hd,text=hd)
+
+
+
 
 GUI.mainloop()
